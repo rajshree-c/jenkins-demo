7549cea8eddab997e0a82b9533c626247c79d8cb
Rajshree Chaurasia <rajshreevc2018@gmail.com>
2026-03-15 21:16:34 +0530

    Testing Jenkins webhook automation

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub and Jenkins!");
+        System.out.println("Hello Jenkins Webhook Test");
     }
 }
